fc8299d17f9bb3a2ccb6649a75a0fbbebd885f81
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-04-01 20:16:49 +0530

    changed endpoint

--- src/components/endpoint.js
+++ src/components/endpoint.js
@@ -1,5 +1,5 @@
 // const FRONTEND_ENDPOINT = 'http://localhost:3001';
-const FRONTEND_ENDPOINT = 'http://localhost:3001';
+const FRONTEND_ENDPOINT = 'https://oven-starvy-pizza.netlify.app';
 // const BACKEND_ENDPOINT = 'http://localhost:3000';
 const BACKEND_ENDPOINT = 'https://oven-starvy-pizza-backend.herokuapp.com';
 
